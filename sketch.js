c1a236eb6282cb32431b1fe1d73c67db1eb133dd
VISHVADHARMAN <67373113+VISHVADHARMAN@users.noreply.github.com>
2020-09-09 09:25:11 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -57,7 +57,7 @@ function setup()
     backgr.scale = 4;
     
     //Flying astronaut
-    astronautFlying =  createSprite(length-150,600,20,20);
+    astronautFlying =  createSprite(length-50,600,20,20);
     astronautFlying.addImage(astronaut_Flying_img);
     astronautFlying.scale = 0.12;
     astronautFlying.visible = true;
